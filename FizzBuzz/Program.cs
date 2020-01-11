fc598b9b7aaec4029223d80e506e3bcbec11030a
1 <1>
2020-01-11 16:06:10 +0500

    Serious Build issue is going to happennn

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -17,7 +17,7 @@ namespace FizzBuzz
             }
         }
 
-        public static string CalculateFizzBuzz(int inpInt32) => inpInt32 switch
+        public static string CalculateFizzBuzz(int inpInt32) => inpInt32 
         {
            //To do : Fix itt
             _ when inpInt32 % 3 == 0 => "Fizz",
