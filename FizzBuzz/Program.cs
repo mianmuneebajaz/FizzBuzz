1c866af70689f55b1ebaa02499c6e6b5761a085f
1 <1>
2020-01-11 15:43:39 +0500

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "0") break;
+                bool showNewLine;
+                var inpInt32 = Convert.ToInt32(input);
+                var data = CalculateFizzBuzz(inpInt32);
+            }
+        }
+
+        public static string CalculateFizzBuzz(int inpInt32) => inpInt32 switch
+        {
+            _ when inpInt32 % 3 == 0 && inpInt32 % 5 == 0 => "FizzBuzz",
+            _ when inpInt32 % 3 == 0 => "Fizz",
+            _ when inpInt32 % 5 == 0 => "Buzz" 
+        };
+    }
+}
---
+++ FizzBuzzTests/ProgramTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static FizzBuzz.Program;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void CalculateFizzBuzzTestFizzWhenInputDivisibleByThree() => AreEqual("Fizz", CalculateFizzBuzz(3));
+
+        [TestMethod]
+        public void CalculateFizzBuzzTestBuzzWhenInputDivisibleByFive() => AreEqual("Buzz", CalculateFizzBuzz(5));
+
+        [TestMethod]
+        public void CalculateFizzBuzzTestFizzBuzzWhenInputDivisibleByThreeAndFive() => AreEqual("FizzBuzz", CalculateFizzBuzz(30));
+    }
+}
